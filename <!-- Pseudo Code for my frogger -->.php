f168860d7009612e39cb28c1f2e6ec6199d05697
jamiesellman <james.sellman12@gmail.com>
2024-02-16 13:04:16 +0000

    start of project scubafrogger. grid has been made and character added to board

---
+++ <!-- Pseudo Code for my frogger -->.php
@@ -0,0 +1,40 @@
+<!-- Pseudo Code for my frogger -->
+<!------------------ HTML ----------------------------------->
+<!-- create a header with h1 (done)-->
+<!-- create a div for score, highscore and lives (done)-->
+<!-- create elements for score highscore and lives most likely a <P> (done)-->
+<!-- create a div container for the grid (done)-->
+<!-- create a div for start button and sound button (done) -->
+<!-- create a start button (done)-->
+<!-- create a toggle sound button (done)-->
+
+<!---------------- JAVASCRIPT ------------------------------>
+<!-- grab grid container and put it in a const (done)-->
+<!-- make nesscary styling in CSS most likely for grid 10x10 (done)-->
+<!-- create conts for cellCount, width and cells for the next function to create a grid (done)-->
+<!-- make a function with a loop that will push cells to the grid div (done)-->
+<!-- create a function to add player character -->
+<!-- create a function to remove player character -->
+<!-- create a function to handle key presses that will make player move and set boundarys -->
+<!-- add styling to grid to divide playable area with start and end zones -->
+<!-- add more styling to decide which rows will have moving hazards and death zones -->
+<!-- create function to add hazards(something like a car) -->
+<!-- create a function that will handle if player or hazard enter the same cell, to remove player -->
+<!-- hazards will move across boards using incriments and find a way to make this happen automatically on a timer , ie every 2 seconds move left one cell -->
+<!-- timings for hazards -->
+
+<!--  create a function to the start game-->
+<!-- lives & display lives -->
+<!--  create a function that will allow the player to scoere 10 points every time the player moves a row forward score & display score-->
+<!-- dymanically display and update the score -->
+<!-- store highscore and compare to player -->
+<!-- create an end game function and displays player score on screen -->
+<!-- reset game function -->
+<!-- how to play window -->
+<!-- sound effects and playlist -->
+
+<!---------------------- CSS --------------------------->
+<!-- set up standard styling ie border & box sizing (done) -->
+<!-- set up grid with styling from example (done) -->
+<!-- set up grid.div width height & border (done)-->
+<!-- style backgrounds -->
--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,27 @@
-console.log("hello");
+const grid = document.querySelector(".grid");
+const width = 10;
+const cellCount = width * width;
+const cells = [];
+const playerCurrentPosition = 95;
+
+//function to create the grid(playing area)
+function createGrid() {
+  //need a forloop to iterate a certain number of times to push the desired number of cells into grid
+  for (let i = 0; i < cellCount; i++) {
+    //create a div and store it in the variable named cell. this will make up a single cell on the board
+    const cell = document.createElement("div");
+    //index to see which cells we are working with. will remove later
+    cell.innerText = i;
+    //grab element we want to push cells into
+    grid.appendChild(cell);
+    cells.push(cell);
+  }
+  addPlayer(playerCurrentPosition);
+}
+
+createGrid();
+
+//function to add player character
+function addPlayer(position) {
+  cells[position].classList.add("player");
+}
