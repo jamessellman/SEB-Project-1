b2a978c7d13aca9c4032f4e661ee85d2ec181429
jamiesellman <james.sellman12@gmail.com>
2024-02-22 19:30:54 +0000

    changed sharks from array to object

--- scripts/app.js
+++ scripts/app.js
@@ -8,11 +8,29 @@ let playerCurrentPosition = 85;
 const lavaPositions = [36, 37, 39, 40, 41, 43, 44, 63, 65, 67, 69, 71];
 const beachPositions = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 const coralPositions = [81, 82, 83, 84, 85, 86, 87, 88, 89];
-//---------------ARRAYS FOR MOVEABLE OBJECTS---------------------//
-let sharkRowOne = [72, 75, 78];
-let sharkRowTwo = [56, 59, 62];
-let sharkRowThree = [27, 29, 31, 33];
-let sharkRowFour = [9, 12, 15];
+//---------------oOBJECT FOR MOVEABLE OBJECTS---------------------//
+
+let sharkRowOne = {
+  start: [72, 75, 78],
+  current: [72, 75, 78],
+  end: 80,
+};
+let sharkRowTwo = {
+  start: [54, 57, 60],
+  current: [54, 57, 60],
+  end: 62,
+};
+let sharkRowThree = {
+  start: [27, 30, 33],
+  current: [27, 30, 33],
+  end: 35,
+};
+let sharkRowFour = {
+  start: [9, 12, 15],
+  current: [9, 12, 15],
+  end: 17,
+};
+
 // -------------TIMER VARIABLES--------------------------------//
 let sharkTimer = null;
 let sharkTimer2 = null;
@@ -46,10 +64,10 @@ function createGrid() {
   addLava(lavaPositions);
   addBeach(beachPositions);
   // addCoral(coralPositions);
-  addShark(sharkRowOne);
-  addShark(sharkRowTwo);
-  addShark(sharkRowThree);
-  addShark(sharkRowFour);
+  addShark(sharkRowOne.start);
+  addShark(sharkRowTwo.start);
+  addShark(sharkRowThree.start);
+  addShark(sharkRowFour.start);
 }
 createGrid();
 //-----------------FUNCTIONS TO ADD & REMOVE PLAYER---------------------//
@@ -107,64 +125,65 @@ function impact() {
 }
 
 //----------- FUNCTIONS TO MAKE SHARKS MOVE--------------------------------------//
-function moveSharkRowOne(interval) {
+function moveSharkRow(interval, sharkRow) {
   sharkTimer = setInterval(() => {
-    removeShark(sharkRowOne);
-    if (sharkRowOne.includes(80)) {
-      sharkRowOne = [72, 75, 78];
+    removeShark(sharkRow.current);
+    if (sharkRow.current.includes(sharkRow.end)) {
+      sharkRow.current = sharkRow.start;
     } else {
-      sharkRowOne = sharkRowOne.map((element) => {
+      console.log(sharkRow);
+      sharkRow.current = sharkRow.current.map((element) => {
         return (element += 1);
       });
     }
-    addShark(sharkRowOne);
+    addShark(sharkRow.current);
     impact();
   }, interval);
 }
 
-function moveSharkRowTwo(interval) {
-  sharkTimer2 = setInterval(() => {
-    removeShark(sharkRowTwo);
-    if (sharkRowTwo.includes(54)) {
-      sharkRowTwo = [56, 59, 62];
-    } else {
-      sharkRowTwo = sharkRowTwo.map((element) => {
-        return (element -= 1);
-      });
-    }
-    addShark(sharkRowTwo);
-    impact();
-  }, interval);
-}
+// function moveSharkRowTwo(interval) {
+//   sharkTimer2 = setInterval(() => {
+//     removeShark(sharkRowTwo);
+//     if (sharkRowTwo.includes(54)) {
+//       sharkRowTwo = [56, 59, 62];
+//     } else {
+//       sharkRowTwo = sharkRowTwo.map((element) => {
+//         return (element -= 1);
+//       });
+//     }
+//     addShark(sharkRowTwo);
+//     impact();
+//   }, interval);
+// }
 
-function moveSharkRowThree(interval) {
-  sharkTimer3 = setInterval(() => {
-    removeShark(sharkRowThree);
-    if (sharkRowThree.includes(35)) {
-      sharkRowThree = [27, 30, 33];
-    } else {
-      sharkRowThree = sharkRowThree.map((element) => {
-        return (element += 1);
-      });
-    }
-    addShark(sharkRowThree);
-    impact();
-  }, interval);
-}
-function moveSharkRowFour(interval) {
-  sharkTimer4 = setInterval(() => {
-    removeShark(sharkRowFour);
-    if (sharkRowFour.includes(17)) {
-      sharkRowFour = [9, 12, 15];
-    } else {
-      sharkRowFour = sharkRowFour.map((element) => {
-        return (element += 1);
-      });
-    }
-    addShark(sharkRowFour);
-    impact();
-  }, interval);
-}
+// function moveSharkRowThree(interval) {
+//   sharkTimer3 = setInterval(() => {
+//     removeShark(sharkRowThree);
+//     if (sharkRowThree.includes(35)) {
+//       sharkRowThree = [27, 30, 33];
+//     } else {
+//       sharkRowThree = sharkRowThree.map((element) => {
+//         return (element += 1);
+//       });
+//     }
+//     addShark(sharkRowThree);
+//     impact();
+//   }, interval);
+// }
+// function moveSharkRowFour(interval) {
+//   sharkTimer4 = setInterval(() => {
+//     removeShark(sharkRowFour);
+//     if (sharkRowFour.includes(17)) {
+//       sharkRowFour = [9, 12, 15];
+//     } else {
+//       sharkRowFour = sharkRowFour.map((element) => {
+//         return (element += 1);
+//       });
+//     }
+//     addShark(sharkRowFour);
+//     impact();
+//   }, interval);
+// }
 
 //------------------FUNCTION TO HANDLE SCORE -----------------------------//
 function handleScore(event) {
@@ -179,10 +198,14 @@ function handleScore(event) {
 }
 // ----------------FUNCTION TO STARTGAME-------------------------------//
 function startGame() {
-  moveSharkRowOne(1000);
-  moveSharkRowTwo(750);
-  moveSharkRowThree(500);
-  moveSharkRowFour(250);
+  moveSharkRow(1000, sharkRowOne);
+  moveSharkRow(750, sharkRowTwo);
+  moveSharkRow(500, sharkRowThree);
+  moveSharkRow(250, sharkRowFour);
+  // moveSharkRowOne(1000);
+  // moveSharkRowTwo(750);
+  // moveSharkRowThree(500);
+  // moveSharkRowFour(250);
   highScoreDisplay.innerText = highScore;
 }
 
@@ -195,6 +218,7 @@ function endGame() {
   removeShark(sharkRowOne);
   removeShark(sharkRowTwo);
   removeShark(sharkRowThree);
+  removeShark(sharkRowFour);
   removePlayer(playerCurrentPosition);
   loseModel();
   console.log("End Game called");
